a7c66998501d5311d983a2d9e6f01bed090aca01
ashit1247 <ashitkumar1247@gmail.com>
2021-05-08 17:01:40 +0530

    React Animation Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -8,6 +8,7 @@ import {
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl'
+import { FadeTransform, Fade, Stagger } from 'react-animation-components'
 
 const required = (val) => val && val.length; //value > 0
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -178,13 +179,18 @@ class DishDetail extends Component {
         if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
-                    <Card>
-                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle> {dish.name}</CardTitle>
-                            <CardText> {dish.description} </CardText>
-                        </CardBody>
-                    </Card>
+                    <FadeTransform in
+                        transformProps={{
+                            exitTransform: 'scale(0.5) translateY(-50%)'
+                        }}>
+                        <Card>
+                            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle> {dish.name}</CardTitle>
+                                <CardText> {dish.description} </CardText>
+                            </CardBody>
+                        </Card>
+                    </FadeTransform>
                 </div>
             );
         }
@@ -201,19 +207,23 @@ class DishDetail extends Component {
         }
         const cmnts = comments.map(comment => {
             return (
+               
                 <li key={comment.id}>
                     <p>{comment.comment}</p>
                     <p>-- {comment.author},
                     {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
                     </p>
                 </li>
+                
             );
         })
         return (
             <div className='col-12 col-md-5 m-1'>
                 <h4> Comments </h4>
+                
                 <ul className='list-unstyled'>
-                    {cmnts}
+                {cmnts}
+                   
                 </ul>
                 <CommentForm comments={comments} dishId={dishId} postComment={postComment} />
             </div>
