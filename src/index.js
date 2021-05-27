7e5fa799676adfde3b06586e3dae5344f48d42d2
ashit1247 <ashitkumar1247@gmail.com>
2021-05-27 23:08:26 +0530

    deleted yarn.lock file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-social/bootstrap-social.css';
